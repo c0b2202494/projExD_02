3739978418f09d5847aaf52f312b00d0548ca2ab
uji <c0b2202494@edu.teu.ac.jp>
2023-04-25 14:22:49 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,6 @@
 import pygame as pg
 import sys
-
+import random
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -8,9 +8,11 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    bom_img = pg.Surface((20,20)) #練習１
-    pg.draw.circle(bom_img, (255,0,0), (10, 10), 10)#練習１
-    bom_img.set_colorkey((0, 0, 0))#練習１
+    bb_img = pg.Surface((20,20)) #練習１
+    pg.draw.circle(bb_img, (255,0,0), (10, 10), 10)#練習１
+    bb_img.set_colorkey((0, 0, 0))#練習１
+    x,y = random.randint(0,1600), random.randint(0,900)#練習２
+    screen.blit(bb_img, [x,y])#練習２
     tmr = 0
 
     while True:
@@ -21,6 +23,7 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bb_img, [x,y])#練習２
 
         pg.display.update()
         clock.tick(1000)
