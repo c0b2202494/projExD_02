0afc56d7b82fa11a9110a67aa45f117d9e36ce4e
uji <c0b2202494@edu.teu.ac.jp>
2023-04-25 16:28:20 +0900

    演習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,7 +1,7 @@
 import pygame as pg
 import sys
 import random
-
+import time
 delta = {pg.K_UP:(0, -1), #練習４
          pg.K_DOWN: (0, 1),
          pg.K_RIGHT: (1, 0),
@@ -38,6 +38,8 @@ def main():
     bb_rct = bb_img.get_rect()#練習３
     bb_rct.center = x, y#練習３
     
+    accs = [a for a in range(1, 11)]
+    bb_imgs = []
     tmr = 0
 
     while True:
@@ -47,11 +49,21 @@ def main():
 
         tmr += 1
 
+
         key_lst = pg.key.get_pressed()#練習４
         for k, mv, in delta.items():#練習４
             if key_lst[k]:#練習４
                 kk_rct.move_ip(mv)#練習４
         
+        for r in range(1, 11):
+            bb_img = pg.Surface((20*r, 20*r))
+            pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+            bb_imgs.append(bb_img)
+
+        avx, avy = vx*accs[min(tmr//1000, 9)], vy*accs[min(tmr//1000, 9)]
+        bb_img = bb_imgs[min(tmr//1000, 9)]
+        bb_img.set_colorkey((0, 0, 0))
+
         if check_bound(screen.get_rect(), kk_rct) != (True, True):#練習５
             for k, mv in delta.items():
                 if key_lst[k]:
@@ -59,13 +71,17 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)#練習４
-        bb_rct.move_ip(vx, vy)#練習３
+        bb_rct.move_ip(avx, avy)#練習３
         yoko, tate = check_bound(screen.get_rect(), bb_rct)#練習５
         if not yoko:  # 横方向にはみ出ていたら#練習５
             vx *= -1
         if not tate:  # 縦方向にはみ出ていたら#練習５
             vy *= -1
         screen.blit(bb_img, bb_rct)#練習３
+
+        if kk_rct.colliderect(bb_rct):  # 練習６
+            return
+        
 
         pg.display.update()
         clock.tick(1000)
