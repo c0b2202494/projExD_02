20057dadcc51340dda3b56bead08b23cf745de8f
uji <c0b2202494@edu.teu.ac.jp>
2023-04-25 15:46:08 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -8,6 +8,17 @@ delta = {pg.K_UP:(0, -1), #練習４
          pg.K_LEFT: (-1, 0),
          }
 
+def check_bound(scr_rct, obj_rct):#練習５
+    '''
+    オブジェクトが画面内にあるか判定し、真理値を返す。
+    （引数１：画面surfaceのrect、引数２：こうかとん、ボムのrect）
+    '''
+    yoko, tate = True, True
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = False
+    return yoko, tate
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -40,10 +51,20 @@ def main():
         for k, mv, in delta.items():#練習４
             if key_lst[k]:#練習４
                 kk_rct.move_ip(mv)#練習４
+        
+        if check_bound(screen.get_rect(), kk_rct) != (True, True):#練習５
+            for k, mv in delta.items():
+                if key_lst[k]:
+                    kk_rct.move_ip(-mv[0], -mv[1])
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)#練習４
         bb_rct.move_ip(vx, vy)#練習３
+        yoko, tate = check_bound(screen.get_rect(), bb_rct)#練習５
+        if not yoko:  # 横方向にはみ出ていたら#練習５
+            vx *= -1
+        if not tate:  # 縦方向にはみ出ていたら#練習５
+            vy *= -1
         screen.blit(bb_img, bb_rct)#練習３
 
         pg.display.update()
